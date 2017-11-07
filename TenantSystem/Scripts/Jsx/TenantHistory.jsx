73487ce21f69cf09591530ceca140b9e204e4655
Kamal Choudhary <kamal_16june@yahoo.co.in>
2017-11-07 23:15:43 +0530

    Create Tenant History component and add it on a viewhistory page

---
+++ TenantSystem/Scripts/Jsx/TenantHistory.jsx
@@ -0,0 +1,55 @@
+﻿class TenantHistory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { tenantHistory: [], selectedTenantId : 0, selectedTenantName : "" };
+        this.getTenantHistory = this.getTenantHistory.bind(this);
+        this.setTenantHistory = this.setTenantHistory.bind(this);
+        this.onTenantSelection = this.onTenantSelection.bind(this);
+    }
+    
+    setTenantHistory(data) {
+        this.setState({
+            tenantHistory: data.bills,
+            selectedTenantName: data.TenantName
+        });
+        console.log(data);
+     }
+
+    getTenantHistory() {        
+        let tenantId = this.state.selectedTenantId;
+
+        var optionGetHistoryOfSelectedTenant = {
+            url: "GetHistoryOfSelectedTenant",
+            datatype: "json",
+            data: { tenantId: tenantId },
+            type: "GET",
+            success: this.setTenantHistory,
+            error: function (xhr, event) {
+                console.log("error");
+            }
+        };
+
+        $.ajax(optionGetHistoryOfSelectedTenant);
+    }
+
+    onTenantSelection(tenantId) {
+        this.setState({
+            selectedTenantId: tenantId
+        }, this.getTenantHistory
+        );        
+    }    
+
+    render() {
+        return (
+            <div>
+                <SelectControl onSelectedItemChange={tenantId => this.onTenantSelection(tenantId)} /><br />
+                <TenantHistoryTable tenantName={this.state.selectedTenantName} tenantHistory={this.state.tenantHistory} />
+            </div>
+        );
+    }
+}
+
+React.render(
+    <TenantHistory />,
+    document.getElementById('ContainerTenantHistory')
+);
---
+++ TenantSystem/Scripts/Jsx/TenantHistoryTable.jsx
@@ -0,0 +1,83 @@
+﻿class TenantHistoryTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { history: props.tenantHistory, tenantName: props.TenantName }
+    }
+
+    render() {
+        const historyData = this.props.tenantHistory;
+        const tenantName = this.props.tenantName;
+        const historyRows = historyData.map((history) => {
+            return (                
+                <HistoryRow classValue="border-tr"
+                    CurrentMonthMeterReadingDate={history.CurrentMonthMeterReadingDate}
+                    CurrentMonthMeterReading={history.CurrentMonthMeterReading}
+                    PreviousMonthMeterReadingDate={history.PreviousMonthMeterReadingDate}
+                    PreviousMonthMeterReading={history.PreviousMonthMeterReading}
+                    PerUnitPrice={history.PerUnitPrice}
+                    AmountPayable={history.AmountPayable}
+                    PaymentDate={history.PaymentDate}
+                    PaymentAmount={history.PaymentAmount}
+                    TotalAmountPayable={history.TotalAmountPayable}
+                />
+            )
+        }
+    );
+
+        return (
+            <div>
+                <h1>{ tenantName }</h1>
+                <table className="tenant-history-template table-bordered table-striped1">
+                    <tbody>
+                    <tr>
+                            <th>Current Month Date
+                                </th>
+                            <th>Current Month Reading
+                                </th>
+                            <th>Previous Month Date
+                                </th>
+                            <th>Previous Month Reading
+                                </th>
+                            <th>Unit Price
+                                </th>
+                            <th>Amount Payable
+                                </th>
+                            <th>Payment Date
+                                </th>
+                            <th>Payment Amount
+                                </th>
+                            <th>Amount Payable
+                                </th>
+                        </tr>
+                {historyRows}
+                </tbody>
+                    </table>
+                </div>
+    );
+}
+}
+
+function HistoryRow(props) {
+    return (
+        <tr className="border-tr1">
+            <td>{moment(props.CurrentMonthMeterReadingDate).format("DD-MMM-YYYY")}
+            </td>
+            <td>{props.CurrentMonthMeterReading }
+            </td>
+            <td>{moment(props.PreviousMonthMeterReadingDate).format("DD-MMM-YYYY")}
+            </td>
+            <td>{props.PreviousMonthMeterReading }
+            </td>
+            <td>{props.PerUnitPrice }
+            </td>
+            <td>{props.AmountPayable }
+            </td>
+            <td>{moment(props.PaymentDate).format("DD-MMM-YYYY")}
+            </td>
+            <td>{props.PaymentAmount }
+            </td>
+            <td>{props.TotalAmountPayable }
+            </td>
+        </tr>
+    );
+}
